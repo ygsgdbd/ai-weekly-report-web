947328aed58d9fc23b7fe291f3c8a90c6ff55f76
ygsgdbd <ygsgdbd@163.com>
2024-08-21 18:10:53 +0800

    fix(next config): 修改资源路径

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: process.env.ASSET_PREFIX,
-  basePath: process.env.BASE_PATH,
+  basePath: "/ai-weekly-report-web",
   images: {
     unoptimized: true,
   },
